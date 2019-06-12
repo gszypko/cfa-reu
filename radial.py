40995ef7785f07ba579fc9cb25e5eec1ccf11532
Greg Szypko <gszypko@gmail.com>
2019-06-12 11:23:45 -0400

    2019-06-12 11:23:45

--- radial.py
+++ radial.py
@@ -3,61 +3,52 @@
 import cdflib
 import numpy as np
 from matplotlib import pyplot as plt
-import datetime
+#import datetime
 from os import listdir
-from scipy.optimize import curve_fit
+#from scipy.optimize import curve_fit
+
+au_km = 1.496e8
 
 dataloc = '/data/reu/gszypko/data/loopback/'
 file_names = listdir(dataloc)
-dat = cdflib.CDF(dataloc+file_names[0])
 
-carrlon = dat.varget('carr_longitude')
-scpos = dat.varget('sc_pos_HCI')
-vp = dat.varget('vp_moment_RTN')
-#n_p = dat.varget('np1_fit')
-dqf = dat.varget('DQF')
-
-for i in range(1,len(file_names)):
-	this_dat = cdflib.CDF(dataloc + file_names[i])
-	this_carrlon = this_dat.varget('carr_longitude')
-	this_scpos = this_dat.varget('sc_pos_HCI')
-	this_vp = this_dat.varget('vp_moment_RTN')
-	#this_n_p = this_dat.varget('np1_fit')
-	this_dqf = this_dat.varget('DQF')
-	carrlon = np.append(carrlon, this_carrlon)
-	scpos = np.append(scpos, this_scpos, axis=0)
-	vp = np.append(vp, this_vp, axis=0)
-	#n_p = np.append(n_p, this_n_p)
-	dqf = np.append(dqf, this_dqf,axis=0)
-
-au_km = 1.496e8
-xloc = scpos[:,0]/au_km
-yloc = scpos[:,1]/au_km
-#zloc = scpos[:,2]/au_km
-zloc = 0 #projecting all positions down into the solar equatorial plane
-dist = np.sqrt(xloc**2 + yloc**2 + zloc**2)
-#n_p.sort()
-#print(n_p)
-#bad = np.where(n_p<=dat.varattsget('np1_fit')['FILLVAL'])
-#n_p[bad]=np.nan
-#print(n_p)
-#n_p[np.where(n_p<0)]=np.nan
-#print(n_p)
-
-#carrlon[bad]=np.nan
-#dist[bad]=np.nan
-
-v_r = vp[:,0]
-#v_t = vp[:,1]
-#v_n = vp[:,2]
-#v_norm = np.sqrt(v_r**2 + v_t**2 + v_n**2)
-
-good = np.where(abs(v_r)<1e12)
-
-fig = plt.figure()
+fig = plt.figure(figsize=(12,9))
 ax = fig.add_subplot(111,projection='polar')
 
-plt.scatter(carrlon[good]*np.pi/180,dist[good],c=v_r[good],cmap='rainbow',s=0.2)
+for i in range(0,len(file_names)):
+	dat = cdflib.CDF(dataloc + file_names[i])
+	carrlon = dat.varget('carr_longitude')
+	carrlat = dat.varget('carr_latitude')
+	scpos = dat.varget('sc_pos_HCI')
+	vp = dat.varget('vp_moment_RTN')
+	n_p = dat.varget('np_moment')	
+
+	xloc = scpos[:,0]/au_km
+	yloc = scpos[:,1]/au_km
+	#zloc = scpos[:,2]/au_km
+	zloc = 0 #projecting all positions down into the solar equatorial plane
+	dist = np.sqrt(xloc**2 + yloc**2 + zloc**2)	
+
+	# plotting radial velocity with reasonable values
+	v_r = vp[:,0]	
+	
+	#good = np.where(abs(v_r)<1e12)
+	good = np.where(abs(n_p) < 1e12)
+	
+	n=50 #step between good data points to plot
+	theta = (carrlon[good]*np.pi/180)[::n]
+	radius = dist[good][::n]
+	color = n_p[good][::n]
+	#color = v_r[good][::n]
+	size = carrlat[good][::n]*30 + 150
+
+	plt.scatter(theta,radius,cmap='jet',c=color,s=size)
+	
 ax.set_rmax(0.5)
-plt.colorbar()
+ax.set_rlabel_position(135-45/2)
+ax.set_title("Estimated proton density in heliocentric corotating frame")
+#ax.set_title("Radial particle velocity in heliocentric corotating frame")
+color_bar = plt.colorbar()
+color_bar.set_label("Proton density (cm^-3)")
+#color_bar.set_label("Radial particle velocity (km/s)")
 plt.show()
